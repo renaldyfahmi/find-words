5e7e5ce5f81741dd3558550b915e885047641705
Adyatma Yusuf Farabi <151417193+1Envy2@users.noreply.github.com>
2024-04-28 20:52:08 +0700

    tugas find words.cpp

--- tugas find words.cpp
+++ tugas find words.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+bool findword(vector<vector<char>>& matriks, string kata) {
+    int baris = matriks.size();
+    int kolom = matriks[0].size();
+
+for (int i = 0; i < baris; ++i) {
+    for (int j = 0; j <= kolom - kata.length(); ++j) {
+        bool ditemukan = true;
+        for (int k = 0; k < kata.length(); ++k) {
+            if (matriks[i][j + k] != kata[k]) {
+                ditemukan = false;
+                break;
+            }
+        }
+        if (ditemukan) return true;
+    }
+}
+
+for (int i = 0; i < baris; ++i) {
+    for (int j = kolom - 1; j >= kata.length() - 1; --j) {
+        bool ditemukan = true;
+        for (int k = 0; k < kata.length(); ++k) {
+            if (matriks[i][j - k] != kata[k]) {
+                ditemukan = false;
+                break;
+            }
+        }
+        if (ditemukan) return true;
+    }
+}
 
